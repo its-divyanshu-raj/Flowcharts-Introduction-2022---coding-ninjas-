084262ebbc05b673649b2f57e0631037f651fe8c
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-05-17 22:19:24 +0530

    Create output1.cpp

---
+++ output1.cpp
@@ -0,0 +1,10 @@
+
+  /* What will be the output of the following code ?  */ 
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "career" << "labs";
+}
+
+ // Correct Answer = careerLabs .
+
